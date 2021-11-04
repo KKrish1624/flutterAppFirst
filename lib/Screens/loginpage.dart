e9ed5d9ff668a03a1b45218704529e03e217797b
krishathreyan <krish.smdn@gmail.com>
2021-11-04 18:14:56 +0530

    Validation added and comments for every step added.

--- lib/Screens/loginpage.dart
+++ lib/Screens/loginpage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/Utils/routes.dart';
+
+//Stateful Widget is used to change the stage of the current page on any action
+//Has two methods, the one that extends from the State class is the one that
+//gives us access to the state of the screen/page
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -11,6 +15,36 @@ class _LoginPageState extends State<LoginPage> {
 
   String name = "";
   bool changeButton = false;
+
+  //Creates a Global key which can be accessed throughout the program
+  final _formKey = GlobalKey<FormState>();
+
+  //This method has the logic of the page navigation
+  moveToHome(BuildContext context) async {
+
+    //From the form key generated, the current state validation is checked
+    //If the validation holds, further steps are carried out.
+    if(_formKey.currentState!.validate()) {
+      //This will give a call back after clicking where we
+      //pass the navigation logic
+      setState(() {
+        changeButton = true;
+      });
+
+      //Waits for the above call back to finish and after the
+      //specified time, performs the next operation
+      await Future.delayed(const Duration(seconds: 2));
+      await Navigator.pushNamed(context, MyRoutes.homeRoute);
+
+      //This will set the changeButton variable to false once it is
+      //navigated to the next screen. When the build function is called
+      //again, the button is reverted back.
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -61,87 +95,121 @@ class _LoginPageState extends State<LoginPage> {
               ),
               Column(
                children: [
+
+                 //Provides symmetric padding i.e vertical padding are same and horizontal padding are same.
                  Padding(
                    padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32.0),
-                   child: Column(
-                     children: [
+                   child: Form(
+                     key: _formKey,
+                     child: Column(
+                       children: [
 
-                       //Email Text Field
-                       TextFormField(
-                         decoration: const InputDecoration(
-                           hintText: "Enter Email",
-                           labelText: "Email",
-                         ),
-                         onChanged: (value){
-                           name = value;
-                           setState(() {});
-                         },
-                       ),
-
-                      //Password Text Field
-                       TextFormField(
-                         obscureText: true,
-                         decoration: const InputDecoration(
-                           hintText: "Enter Password",
-                           labelText: "Password",
-                         ),
-                       ),
-
-                       const SizedBox(
-                         height: 20.0,
-                       ),
-
-                       //Button for logging in.
-                       // ElevatedButton(
-                       //     onPressed: () {
-                       //       Navigator.pushNamed(context, MyRoutes.homeRoute);
-                       //     },
-                       //     //style: TextButton.styleFrom(),
-                       //     style: TextButton.styleFrom(
-                       //       minimumSize: const Size(130, 45),
-                       //     ),
-                       //     child: const Text("Login")
-                       // )
-
-                       //Widget to give a container clickable property
-                       //Alternative for InkWell is GesterDetector
-                       //The later does not give the clicking property or feedback
-                       //Only Tap is available.
-                       InkWell(
-
-                         //This will give a call back after clicking where we
-                         //pass the navigation logic
-                         onTap:  () async {
-                           setState(() {
-                             changeButton = true;
-                           });
-                           await Future.delayed(const Duration(seconds: 1));
-                           Navigator.pushNamed(context, MyRoutes.homeRoute);
-                         },
-                         child: AnimatedContainer(
-
-                           //This duration is the time taken before the animation
-                           //is performed.
-                           duration: const Duration(seconds: 1),
-                           width: changeButton? 40: 130,
-                           height: 45,
-                           alignment: Alignment.center,
-                           child: changeButton? const Icon(
-                               Icons.done, color: Colors.white,
-                           ) : const Text("Login",
-                           style: TextStyle(
-                             color: Colors.white,
-                             fontSize: 16,
-                             fontWeight: FontWeight.bold
+                         //Email Text Field
+                         TextFormField(
+                           decoration: const InputDecoration(
+                             hintText: "Enter Email",
+                             labelText: "Email",
                            ),
+
+                            //This is a Validator function which validates the value that is
+                           //input. If the validation holds, will return null else, will
+                           //return a message.
+                            validator: (value){
+                             if(value!.isEmpty){
+                              return "Username can't be empty!";
+                             }else{
+                              return null;
+                             }
+                            },
+                           //Monitors any change in the text field, performs the callback in case of a change
+                           onChanged: (value){
+                             name = value;
+
+                             //Refreshes the state of the page/screen and performs the action specifies in.
+                             //It basically calls the build function again.
+                             setState(() {});
+                           },
+                         ),
+
+                        //Password Text Field
+                         TextFormField(
+
+                           //Is used to hide the entered text in the field.
+                           obscureText: true,
+                           decoration: const InputDecoration(
+                             hintText: "Enter Password",
+                             labelText: "Password",
                            ),
-                           decoration: BoxDecoration(
+                           validator: (value){
+                             if(value!.isEmpty){
+                               return "Password can't be empty!";
+                             }else{
+                               return null;
+                             }
+                           },
+                         ),
+
+                         const SizedBox(
+                           height: 20.0,
+                         ),
+
+                         //Button for logging in.
+                         // ElevatedButton(
+                         //     onPressed: () {
+                         //       Navigator.pushNamed(context, MyRoutes.homeRoute);
+                         //     },
+                         //     //style: TextButton.styleFrom(),
+                         //     style: TextButton.styleFrom(
+                         //       minimumSize: const Size(130, 45),
+                         //     ),
+                         //     child: const Text("Login")
+                         // )
+
+                         //Widget to give a container clickable property
+                         //Alternative for InkWell is GesterDetector
+                         //The later does not give the clicking property or feedback
+                         //Only Tap is available.
+                         //To get the repel effect while clicking, we must make sure
+                         //the parent widget of InkWell must a Material Widget and
+                         //the decoration must be given to the Material Widget.
+                         Material(
                              color: Colors.deepPurple,
+                             //Controls the border radius of the container.
                              borderRadius: BorderRadius.circular(changeButton?20:8),
+                           child: InkWell(
+                             //splashColor: Colors.green,
+                               onTap:  () => moveToHome(context),
+
+                               //Making a button with simple animation from a container
+
+                             //Performs specified action in an animated way
+                             child: AnimatedContainer(
+
+                               //This duration is the time taken before the animation
+                               //is performed.
+                               duration: const Duration(seconds: 1),
+                               width: changeButton? 40: 130,
+                               height: 45,
+                               alignment: Alignment.center,
+
+                               //Icons pack of Flutter
+                               //Icon.done gives a tick
+                               child: changeButton? const Icon(
+                                   Icons.done, color: Colors.white,
+                               ) : const Text("Login",
+                               style: TextStyle(
+                                 color: Colors.white,
+                                 fontSize: 16,
+                                 fontWeight: FontWeight.bold
+                               ),
+                               ),
+                               // decoration: BoxDecoration(
+                               // ),
+                             ),
                            ),
-                         ),
-                       )
-                     ],
+                         )
+                       ],
+                     ),
                    ),
                  )
                ],
